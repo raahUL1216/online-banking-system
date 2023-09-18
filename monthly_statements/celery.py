87234342e33eb7305d9a28ed788e2c55bf9f4bdd
Rahul <rahulchodvadiya@gmail.com>
2023-09-18 05:46:00 +0530

    update docker compose for scheduler

--- monthly_statements/celery.py
+++ monthly_statements/celery.py
@@ -1,5 +1,4 @@
 import os
-from venv import logger
 from celery import Celery
 from celery.schedules import crontab
 
@@ -7,20 +6,16 @@ celery_broker_url = os.environ.get('CELERY_BROKER_URL', '')
 
 app = Celery(
     'monthly_statements',
-    broker=celery_broker_url,
-    include=['monthly_statements.tasks']
+    broker=celery_broker_url
 )
 
-logger.info("before cron")
 # Generate monthly statement for all users on the 1st day of every month
 app.conf.beat_schedule = {
     'generate-monthly-statements': {
         'task': 'monthly_statements.tasks.generate_monthly_statements',
-        'schedule': crontab(minute=1),
+        'schedule': crontab(minute='*')
     },  
 }
 
-print(__name__)
 if __name__ == '__main__':
-    logger.info("in main.py")
     app.start()
--- monthly_statements/tasks.py
+++ monthly_statements/tasks.py
@@ -1,4 +1,3 @@
-from venv import logger
 from .celery import app
 
 from database import get_db
@@ -9,17 +8,16 @@ from datetime import datetime, timedelta
 
 db = get_db()
 
-@app.task
+@app.task(bind=True, name="generate_monthly_statements")
 def generate_monthly_statements():
     '''
     Retrieve users and create monthly statement for each user
     '''
-    logger.info("in tasks.py")
-    # users = User.query.all()
+    users = User.query.all()
     
-    # today = datetime.now()
-    # previous_month = (today.replace(day=1) - timedelta(days=1)).strftime('%B')
-    # current_year = today.strftime('%Y')
+    today = datetime.now()
+    previous_month = (today.replace(day=1) - timedelta(days=1)).strftime('%B')
+    current_year = today.strftime('%Y')
 
-    # for user in users:
-    #     generate_monthly_statement(user.user_id, previous_month, current_year)
+    for user in users:
+        generate_monthly_statement(user.user_id, previous_month, current_year)
